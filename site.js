dbec005f14802ad88ccd7dcc578adf4abadc575f
Szensiker <instabeercaps@gmail.com>
2017-07-02 11:40:51 +0200

    data loading with img formating

--- site.js
+++ site.js
@@ -1,5 +1,14 @@
 ﻿$(document).ready(function() {
     $('#example').DataTable( {
-        "ajax": 'https://raw.githubusercontent.com/Szensiker/CapsCollectorData/master/caps.txt'
+        "ajax": "https://raw.githubusercontent.com/Szensiker/CapsCollectorData/master/caps.txt",
+        "columns": [
+            { 
+                "data": function(data){ return "<img src='" + data["image"] +"' height='100' width='100'>"; },
+                "width" : "84px"     
+            },
+            { "data": "beer_name" },
+            { "data": "country" },
+            { "data": "colours" }
+        ]
     } );
 } );
